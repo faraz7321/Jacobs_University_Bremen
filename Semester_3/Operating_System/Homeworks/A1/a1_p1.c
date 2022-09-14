2f570ef9ce8dbd7e1da2066b49fda2ca25f7d569
Faraz Ahmad <81792548+faraz7321@users.noreply.github.com>
2022-09-14 15:17:06 +0200

    p1

---
+++ Semester_3/Operating_System/Homeworks/A1/a1_p1.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+char *strdup(const char *s)
+{
+    size_t len = strlen(s);
+
+    // char d[len + 1]; // value of len needs to be constant.
+    char *d = malloc(len +1);
+    return strncpy(d, s, len + 1);
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 1; i < argc; i++)
+    { 
+        (void)puts(strdup(argv[i]));
+    }
+
+    return EXIT_SUCCESS;
+}
