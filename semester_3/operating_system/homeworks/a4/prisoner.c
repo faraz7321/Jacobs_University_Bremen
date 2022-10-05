31106c21d44ebd707618b862c04b7e2c2d46c527
Faraz Ahmad <faraz7321@gmail.com>
2022-10-05 02:18:36 +0200

    strategy_drawer not working

--- semester_3/operating_system/homeworks/a4/prisoner.c
+++ semester_3/operating_system/homeworks/a4/prisoner.c
@@ -17,23 +17,7 @@
 #include <stdarg.h>
 #include <time.h>
 
-pthread_mutex_t *mutex;
-static const char *progname = "prisoner";
-static uint32_t m_in_critical_region = 0;
-
-#define CRITICAL_SECTION_ENTER()   \
-    {                              \
-        pthread_mutex_lock(mutex); \
-        ++m_in_critical_region;    \
-    }
-
-#define CRITICAL_SECTION_EXIT()          \
-    {                                    \
-        if (--m_in_critical_region == 0) \
-        {                                \
-            pthread_mutex_unlock(mutex); \
-        }                                \
-    }
+#define PRISONERS 100
 
 typedef struct
 {
@@ -41,27 +25,47 @@ typedef struct
     bool opened;
 } drawer_t;
 
+/** GLOBAL VARIABLES **/
+static const char *progname = "prisoner";
+
+// mutexes
+pthread_mutex_t mutex;
+pthread_mutex_t mutexes[100];
+
+// program specific
+drawer_t *drawers;
+static uint64_t wins = 0;
+bool victory = true;
+
+/** FUNCTION PROTOTYPES **/
+// thread functions
 static double timeit(int n, void *(*proc)(void *));
 void run_threads(int n, void *(*proc)(void *));
 
+// utility functions
 void initialize(int num_of_prisoners);
+void resetDrawers(int num_of_prisoners);
+void run_games(char *game, void *(*proc)(void *), uint64_t n);
+
+// routines
 void *random_drawer(void *ptr);
 void *random_global(void *ptr);
 void *strategy_drawer(void *ptr);
 void *strategy_global(void *ptr);
 
-drawer_t *drawers;
-const int num_of_prisoners = 100;
-static uint64_t wins = 0;
-
 int main(int argc, char **argv)
 {
+
     srand(time(NULL));
-    pthread_mutex_init(mutex, NULL);
+
+    for (int i = 0; i < PRISONERS; ++i)
+    {
+        pthread_mutex_init(&mutexes[i], NULL);
+    }
+    pthread_mutex_init(&mutex, NULL);
 
     uint64_t num_of_games = 100;
     int seed;
-    char *end;
     int c;
 
     while ((c = getopt(argc, argv, "n:s:h")) != -1)
@@ -95,35 +99,33 @@ int main(int argc, char **argv)
             break;
         }
     }
+    // printf("starting\n");
+    run_games("random_drawer  ", random_drawer, num_of_games);
+    run_games("random_global  ", random_global, num_of_games);
+    // run_games("strategy_drawer", strategy_drawer, num_of_games);
+    run_games("strategy_global", strategy_global, num_of_games);
 
-    for (uint64_t i = 0; i < num_of_games; i++)
-    {
-        initialize(num_of_prisoners);
-        // wins += strategy...?
-    }
-    double time = 0.0;
-
-    printf("method random_global    %9lu/%lu = %3.2lf%%  %3.3lf ms\n", wins, num_of_games, (100.0 * wins) / num_of_games, time);
-    printf("method random_drawer    %9lu/%lu = %3.2lf%%  %3.3lf ms\n", wins, num_of_games, (100.0 * wins) / num_of_games, time);
-    printf("method strategy_global  %9lu/%lu = %3.2lf%%  %3.3lf ms\n", wins, num_of_games, (100.0 * wins) / num_of_games, time);
-    printf("method strategy_drawer  %9lu/%lu = %3.2lf%%  %3.3lf ms\n", wins, num_of_games, (100.0 * wins) / num_of_games, time);
     return EXIT_SUCCESS;
 }
-
-static double timeit(int n, void *(*proc)(void *))
+void run_games(char *game, void *(*proc)(void *), uint64_t n)
 {
-    clock_t t1, t2;
-    t1 = clock();
-    run_threads(n, proc);
-    t2 = clock();
-    return ((double)t2 - (double)t1) / CLOCKS_PER_SEC * 1000;
+    wins = 0;
+    double time = 0.0;
+    for (int i = 0; i < n; ++i)
+    {
+        initialize(PRISONERS);
+        time += timeit(PRISONERS, proc);
+    }
+    printf("method %s     %9lu/%lu = %2.2lf%%  %3.3lf ms\n", game, wins, n, (100.0 * wins) / n, time);
 }
+
 void initialize(int num_of_prisoners)
 {
+    free(drawers);
     int j, card;
     bool unique;
 
-    drawers = ((drawer_t *)malloc(num_of_prisoners * sizeof(drawer_t))) - 1;
+    drawers = ((drawer_t *)malloc(num_of_prisoners * sizeof(drawer_t)));
 
     card = rand() % num_of_prisoners + 1;
     drawers[0] = (drawer_t){.card = card, .opened = false};
@@ -149,185 +151,190 @@ void initialize(int num_of_prisoners)
         drawers[i] = (drawer_t){.card = card, .opened = false};
     }
 }
+static double timeit(int n, void *(*proc)(void *))
+{
+    clock_t t1, t2;
+    t1 = clock();
+    run_threads(n, proc);
+    t2 = clock();
+    return ((double)t2 - (double)t1) / CLOCKS_PER_SEC * 1000;
+}
 void run_threads(int n, void *(*proc)(void *))
 {
     pthread_t id[n];
+    victory = true;
     // creating the child threads
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        pthread_create(&id[i - 1], NULL, proc, NULL);
+        pthread_create(&id[i], NULL, proc, &i);
     }
 
     // joining the threads one by one
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        pthread_join(id[i - 1], NULL);
+        pthread_join(id[i], NULL);
+    }
+
+    if (victory)
+    {
+        wins += 1;
     }
 }
 void *random_drawer(void *ptr)
 {
     int select;
+    int *i = (int *)ptr;
+    bool foundCard = false;
 
-    for (int i = 0; i < num_of_prisoners; i++)
+    bool is_drawer_opened[100];
+    for (int j = 0; j < 100; j++)
     {
-        bool foundCard = false;
-        for (int j = 0; j < 50; j++)
-        {
-            do
-            {
-                select = rand() % num_of_prisoners + 1;
-            } while (drawers[select].opened == true);
-
-            CRITICAL_SECTION_ENTER();
-
-            if (drawers[select].card == i)
-            {
-                foundCard = true;
-                break;
-            }
-            drawers[select].opened = true;
-
-            CRITICAL_SECTION_EXIT();
-        }
-        for (int i = 0; i < num_of_prisoners; i++)
-        {
-            drawers[i].opened = false;
-        }
-        if (foundCard == false)
-        {
-            // return true;
-        }
+        is_drawer_opened[j] = false;
     }
+    // printf("innnn %d\n", *i);
+    for (int j = 0; j < 50; j++)
+    {
+        do
+        {
+            select = rand() % PRISONERS + 1;
+        } while (is_drawer_opened[select]);
+        // printf("in %d\n", j);
+        pthread_mutex_lock(&mutexes[select]);
 
-    // return false;
+        if (drawers[select].card == *i)
+        {
+            foundCard = true;
+            // printf("found\n");
+            pthread_mutex_unlock(&mutexes[select]);
+            break;
+        }
+        is_drawer_opened[select] = true;
+        pthread_mutex_unlock(&mutexes[select]);
+    }
+    if (!foundCard)
+    {
+        victory = false;
+    }
 }
+
 void *random_global(void *ptr)
 {
+    int *i = (int *)ptr;
     int select;
 
-    for (int i = 0; i < num_of_prisoners; i++)
+    pthread_mutex_lock(&mutex);
+
+    bool foundCard = false;
+    for (int j = 0; j < 50; j++)
     {
-        CRITICAL_SECTION_ENTER();
-
-        bool foundCard = false;
-        for (int j = 0; j < 50; j++)
+        do
         {
-            do
-            {
-                select = rand() % num_of_prisoners + 1;
-            } while (drawers[select].opened == true);
+            select = rand() % PRISONERS + 1;
+        } while (drawers[select].opened == true);
 
-            if (drawers[select].card == i)
-            {
-                foundCard = true;
-                break;
-            }
-            drawers[select].opened = true;
-        }
-
-        for (int i = 0; i < num_of_prisoners; i++)
+        if (drawers[select].card == *i)
         {
-            drawers[i].opened = false;
+            foundCard = true;
+            break;
         }
-
-        CRITICAL_SECTION_EXIT();
-
-        if (foundCard == false)
-        {
-            // return true;
-        }
+        drawers[select].opened = true;
     }
 
-    // return false;
+    resetDrawers(PRISONERS);
+
+    if (!foundCard)
+    {
+        victory = false;
+    }
+    pthread_mutex_unlock(&mutex);
 }
+
 void *strategy_drawer(void *ptr)
 {
     int select;
-
+    int *i = (int *)ptr;
     bool foundCard = false;
-    for (int i = 0; i < num_of_prisoners; i++)
+    bool is_drawer_opened[100];
+    for (int j = 0; j < 100; j++)
     {
-        select = i;
-        foundCard = false;
-        for (int j = 0; j < 50; j++)
+        is_drawer_opened[j] = false;
+    }
+    select = *i;
+    foundCard = false;
+    for (int j = 0; j < 50; j++)
+    {
+        pthread_mutex_lock(&mutexes[select]);
+        is_drawer_opened[select] = true;
+
+        if (drawers[select].card == *i)
         {
-            CRITICAL_SECTION_ENTER();
-            drawers[select].opened = true;
-
-            if (drawers[select].card == i)
+            foundCard = true;
+            pthread_mutex_unlock(&mutexes[select]);
+            break;
+        }
+        if (select == drawers[select].card)
+        {
+            do
             {
-                foundCard = true;
-                break;
-            }
-            if (select == drawers[select].card)
-            {
-                do
-                {
-                    select = rand() % num_of_prisoners + 1;
-                } while (drawers[select].opened == true);
-            }
-            else
-            {
-                select = drawers[select].card;
-            }
-
-            CRITICAL_SECTION_EXIT();
+                select = rand() % PRISONERS + 1;
+            } while (is_drawer_opened[select]);
+        }
+        else
+        {
+            select = drawers[select].card;
         }
 
-        for (i = 0; i < num_of_prisoners; i++)
-        {
-            drawers[i].opened = false;
-        }
-        if (foundCard == false)
-        {
-            // return true;
-        }
+        pthread_mutex_unlock(&mutexes[select]);
     }
 
-    // return false;
+    if (!foundCard)
+    {
+        victory = false;
+    }
 }
+
 void *strategy_global(void *ptr)
 {
+    int *i = (int *)ptr;
     int select;
 
     bool foundCard = false;
-    for (int i = 0; i < num_of_prisoners; i++)
+    pthread_mutex_lock(&mutex);
+    select = *i;
+    foundCard = false;
+    for (int j = 0; j < 50; j++)
     {
-        CRITICAL_SECTION_ENTER();
-        select = i;
-        foundCard = false;
-        for (int j = 0; j < 50; j++)
-        {
-            drawers[select].opened = true;
+        drawers[select].opened = true;
 
-            if (drawers[select].card == i)
-            {
-                foundCard = true;
-                break;
-            }
-            if (select == drawers[select].card)
-            {
-                do
-                {
-                    select = rand() % num_of_prisoners + 1;
-                } while (drawers[select].opened == true);
-            }
-            else
-            {
-                select = drawers[select].card;
-            }
-        }
-
-        for (i = 0; i < num_of_prisoners; i++)
+        if (drawers[select].card == *i)
         {
-            drawers[i].opened = false;
+            foundCard = true;
+            break;
         }
-        CRITICAL_SECTION_EXIT();
-        if (foundCard == false)
+        if (select == drawers[select].card)
         {
-            // return true;
+            do
+            {
+                select = rand() % PRISONERS + 1;
+            } while (drawers[select].opened == true);
+        }
+        else
+        {
+            select = drawers[select].card;
         }
     }
 
-    // return false;
+    resetDrawers(PRISONERS);
+    if (!foundCard)
+    {
+        victory = false;
+    }
+    pthread_mutex_unlock(&mutex);
+}
+void resetDrawers(int n)
+{
+    for (int j = 0; j < n; j++)
+    {
+        drawers[j].opened = false;
+    }
 }
