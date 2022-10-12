afbe2823f88c8fcf97e600cb12a888632099016e
Faraz Ahmad <faraz7321@gmail.com>
2022-10-12 22:51:40 +0200

    hw5

--- semester_3/operating_system/homeworks/a5/account.c
+++ semester_3/operating_system/homeworks/a5/account.c
@@ -21,7 +21,7 @@ typedef struct arg_data
 } arg_data_t;
 
 static double timeit(int n, void *(*proc)(void *));
-void run_threads(int num_of_prisoners, void *(*proc)(void *), arg_data_t args[]);
+void run_threads(int num_of_prisoners, void *(*proc)(void *));
 void *transfer(void *ptr);
 void __init();
 account_t from;
@@ -29,11 +29,9 @@ account_t to;
 
 int main()
 {
-    unsigned int money = 100;
+    int thread_num = 6; // number of threads
     __init();
-    // printf("INITIALIZED\n");
-    arg_data_t args = {.from = &from, .to = &to, .money = money};
-    run_threads(1, transfer, &args);
+    run_threads(thread_num, transfer);
     printf("from: %u\nmoney: %u\n", from.number, from.money);
     printf("to: %u\nmoney: %u\n", to.number, to.money);
 }
@@ -45,18 +43,32 @@ void __init()
     to.number = 2;
     to.money = 10000;
     pthread_mutex_init(&to.lock, NULL);
+    printf("INITIALIZED\n");
 }
-void run_threads(int num, void *(*proc)(void *), arg_data_t args[])
+void run_threads(int num, void *(*proc)(void *))
 {
-
+    unsigned int money = 100;
+    arg_data_t args;
     pthread_t id[num];
     // creating the child threads
-    // printf("Creating threads\n");
+    printf("Creating threads\n");
     for (int i = 0; i < num; i++)
     {
-        pthread_create(&id[i], NULL, proc, &args[i]);
+        if (i % 2 == 0)
+        {
+            args.to = &from;
+            args.from = &to;
+        }
+        else
+        {
+            args.to = &to;
+            args.from = &from;
+        }
+        args.money = money;
+
+        pthread_create(&id[i], NULL, proc, &args);
     }
-    // printf("Joining threads\n");
+    printf("Joining threads\n");
     //  joining the threads one by one
     for (int i = 0; i < num; i++)
     {
@@ -68,10 +80,12 @@ void *transfer(void *ptr)
     arg_data_t *args = (arg_data_t *)ptr;
 
     // results in deadlock
+
     pthread_mutex_lock(&args->from->lock);
-    pthread_mutex_lock(&args->to->lock);
     args->from->money -= args->money;
+    pthread_mutex_unlock(&args->from->lock);
+
+    pthread_mutex_lock(&args->to->lock);
     args->to->money += args->money;
     pthread_mutex_unlock(&args->to->lock);
-    pthread_mutex_unlock(&args->from->lock);
 }
