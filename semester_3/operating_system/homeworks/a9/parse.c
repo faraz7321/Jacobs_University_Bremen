dd82c25039d11fc598ce0b6f0353170dd9f33cb5
Faraz Ahmad <faraz7321@gmail.com>
2022-11-08 16:04:01 +0100

    a9

---
+++ semester_3/operating_system/homeworks/a9/parse.c
@@ -0,0 +1,13 @@
+#include "quiz.h"
+/**
+ * @brief parses the JSON input and adds the next question
+ *  and the expected answer to the quiz data structure
+ *
+ * @param json
+ * @param quiz
+ * @return int
+ */
+int parse(char *json, quiz_t *quiz)
+{
+    return 1;
+}
---
+++ semester_3/operating_system/homeworks/a9/play.c
@@ -0,0 +1,10 @@
+#include "quiz.h"
+/**
+ * @brief plays one iteration of the game
+ * 
+ * @param quiz 
+ * @return int 
+ */
+int play(quiz_t *quiz){
+
+}
