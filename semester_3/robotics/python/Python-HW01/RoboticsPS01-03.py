51da7afd842e752efeb6eee2ece5c6c5c235e2f7
faraz7321 <faraz7321@gmail.com>
2022-11-10 13:16:04 +0100

    hw1

--- semester_3/robotics/python/Python-HW01/RoboticsPS01-03.py
+++ semester_3/robotics/python/Python-HW01/RoboticsPS01-03.py
@@ -12,10 +12,10 @@ np.set_printoptions(precision=4, suppress=True)
 #
 
 # option 1 : initialize quaternion instance on creation
-q1 = rob.Quat(np.array([1,2,3,4]))
+q1 = rob.Quat(np.array([1, 2, 3, 4]))
 
-# option 2 : 
-# - create quaternion instance, 
+# option 2 :
+# - create quaternion instance,
 # - then set its components with the according method
 q2 = rob.Quat()
 q2.set_q(0.4811480, 0.1984591, 0.7246066, 0.4517253)
@@ -23,12 +23,12 @@ q2.set_q(0.4811480, 0.1984591, 0.7246066, 0.4517253)
 # compute quaternion norms and print them
 n1 = q1.norm()
 n2 = q2.norm()
-print('quaternion q1\n', q1, '\n')    
+print('quaternion q1\n', q1, '\n')
 print('norm of quaternion q1\n', n1, '\n')
-print('quaternion q2\n', q2, '\n')    
-print('norm of quaternion q2\n', n2, '\n')    
+print('quaternion q2\n', q2, '\n')
+print('norm of quaternion q2\n', n2, '\n')
 
 # compute quaternion product and print it
 q3 = q2 @ q1
 print('\n')
-print('quaternion product of q1 and q2 \n', q3, '\n')    
+print('quaternion product of q1 and q2 \n', q3, '\n')
