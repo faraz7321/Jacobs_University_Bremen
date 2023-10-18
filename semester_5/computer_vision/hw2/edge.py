724cabb4bdd05ce01cb674ec49a367fdd8e1858a
Faraz Ahmad <Faraz7321@gmail.com>
2023-10-18 19:00:07 +0200

    done

--- semester_5/computer_vision/hw2/edge.py
+++ semester_5/computer_vision/hw2/edge.py
@@ -27,7 +27,15 @@ def conv(image, kernel):
     padded = np.pad(image, pad_width, mode='edge')
 
     ### YOUR CODE HERE
-    pass
+    for i in range(Hi):
+        for j in range(Wi):
+            # Extract the window from the padded image
+            window = padded[i:i + Hk, j:j + Wk]
+            
+            # Element-wise multiplication and summing up
+            conv_value = np.sum(window * kernel)
+            
+            out[i, j] = conv_value
     ### END YOUR CODE
 
     return out
@@ -52,7 +60,18 @@ def gaussian_kernel(size, sigma):
     kernel = np.zeros((size, size))
 
     ### YOUR CODE HERE
-    pass
+    
+    # center of the kernel
+    center = size // 2
+
+    for i in range(size):
+        for j in range(size):
+            # Calculating the x, y coordinates relative to the center of the kernel
+            x, y = i - center, j - center
+            
+            # Calculating the gaussian value
+            kernel[i, j] = (1 / (2 * np.pi * sigma ** 2)) * np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+
     ### END YOUR CODE
 
     return kernel
@@ -72,7 +91,10 @@ def partial_x(image):
     out = None
 
     ### YOUR CODE HERE
-    pass
+     # central difference x-kernel
+    D_x = np.array([[-0.5, 0, 0.5]])
+
+    out = conv(image, D_x)
     ### END YOUR CODE
 
     return out
@@ -92,7 +114,12 @@ def partial_y(image):
     out = None
 
     ### YOUR CODE HERE
-    pass
+    # central difference y-kernel
+    D_y = np.array([[-0.5], 
+                   [0], 
+                   [0.5]])
+
+    out = conv(image, D_y)
     ### END YOUR CODE
 
     return out
@@ -116,7 +143,11 @@ def gradient(image):
     theta = np.zeros(image.shape)
 
     ### YOUR CODE HERE
-    pass
+    G_x = partial_x(image)
+    G_y = partial_y(image)
+
+    G = np.sqrt(G_x ** 2 + G_y ** 2)
+    theta = (np.degrees(np.arctan2(G_y, G_x)) + 360) % 360
     ### END YOUR CODE
 
     return G, theta
@@ -138,11 +169,27 @@ def non_maximum_suppression(G, theta):
     H, W = G.shape
     out = np.zeros((H, W))
 
-    # Round the gradient direction to the nearest 45 degrees
-    theta = np.floor((theta + 22.5) / 45) * 45
-
     ### BEGIN YOUR CODE
-    pass
+    # Round the gradient direction to the nearest 45 degrees
+    theta = (np.floor((theta + 22.5) / 45) * 45) % 360
+
+    # Define directions for 8-connected neighborhood
+    directions = {0: (0, 1),
+                45: (1, 1),
+                90: (1, 0),
+                135: (1, -1),
+                180: (0, -1),
+                225: (-1, -1),
+                270: (-1, 0),
+                315: (-1, 1)}
+
+    for i in range(1, H - 1):
+        for j in range(1, W - 1):
+            angle = theta[i, j]
+            dx, dy = directions[angle]
+            if G[i, j] >= G[i + dx, j + dy] and G[i, j] >= G[i - dx, j - dy]:
+                out[i, j] = G[i, j]
+
     ### END YOUR CODE
 
     return out
@@ -163,11 +210,12 @@ def double_thresholding(img, high, low):
             higher threshold and greater than the lower threshold.
     """
 
-    strong_edges = np.zeros(img.shape, dtype=np.bool)
-    weak_edges = np.zeros(img.shape, dtype=np.bool)
+    strong_edges = np.zeros(img.shape, dtype=bool) #np.bool is depriciated
+    weak_edges = np.zeros(img.shape, dtype=bool)
 
     ### YOUR CODE HERE
-    pass
+    strong_edges = img > high
+    weak_edges = (img <= high) & (img > low)
     ### END YOUR CODE
 
     return strong_edges, weak_edges
@@ -218,7 +266,7 @@ def link_edges(strong_edges, weak_edges):
 
     H, W = strong_edges.shape
     indices = np.stack(np.nonzero(strong_edges)).T
-    edges = np.zeros((H, W), dtype=np.bool)
+    edges = np.zeros((H, W), dtype=bool)
 
     # Make new instances of arguments to leave the original
     # references intact
@@ -226,7 +274,25 @@ def link_edges(strong_edges, weak_edges):
     edges = np.copy(strong_edges)
 
     ### YOUR CODE HERE
-    pass
+
+    neighbors = [(0, 1), (1, 0), (0, -1), (-1, 0), 
+                 (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    queue = list(indices)
+
+    # Breadth-First Search
+    while queue:
+        i, j = queue.pop(0)
+
+        for dx, dy in neighbors:
+            x, y = i + dx, j + dy
+
+            if x >= 0 and x < H and y >= 0 and y < W:
+                if weak_edges[x, y]:
+                    edges[x, y] = True
+                    weak_edges[x, y] = False
+                    queue.append((x, y))
+
     ### END YOUR CODE
 
     return edges
@@ -244,7 +310,23 @@ def canny(img, kernel_size=5, sigma=1.4, high=20, low=15):
         edge: numpy array of shape(H, W).
     """
     ### YOUR CODE HERE
-    pass
+    
+     # Smooth the image using Gaussian filter
+    kernel = gaussian_kernel(kernel_size, sigma)
+    smoothed_img = conv(img, kernel)
+
+    # intensity gradients
+    G, theta = gradient(smoothed_img)
+
+    # non-maximum suppression
+    nms_img = non_maximum_suppression(G, theta)
+
+    # double Thresholding
+    strong_edges, weak_edges = double_thresholding(nms_img, high, low)
+
+    # edge tracking by hysteresis
+    edge = link_edges(strong_edges, weak_edges)
+
     ### END YOUR CODE
 
     return edge
@@ -268,7 +350,7 @@ def hough_transform(img):
     # Set rho and theta ranges
     W, H = img.shape
     diag_len = int(np.ceil(np.sqrt(W * W + H * H)))
-    rhos = np.linspace(-diag_len, diag_len, diag_len * 2.0 + 1)
+    rhos = np.linspace(-diag_len, diag_len, int(diag_len * 2.0 + 1))
     thetas = np.deg2rad(np.arange(-90.0, 90.0))
 
     # Cache some reusable values
@@ -284,7 +366,14 @@ def hough_transform(img):
     # Find rho corresponding to values in thetas
     # and increment the accumulator in the corresponding coordinate
     ### YOUR CODE HERE
-    pass
+    for i in range(len(xs)):
+        x = xs[i]
+        y = ys[i]
+
+        for theta in range(num_thetas):
+            # Calculate rho. diag_len is added for a positive index
+            rho = int(round(x * cos_t[theta] + y * sin_t[theta]) + diag_len)
+            accumulator[rho, theta] += 1
     ### END YOUR CODE
 
     return accumulator, rhos, thetas
